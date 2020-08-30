4e7cc758f39059653b203ac8c88a117c5e558734
PWiener <patrykwiener@gmail.com>
2020-08-30 16:18:06 +0200

    added passage class

---
+++ models/passage.py
@@ -0,0 +1,26 @@
+"""This module contains Passage model class reflecting database's 'przebiegi' table."""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import backref, relationship
+
+from settings import Base
+
+
+class Passage(Base):
+    """Reflects 'przebiegi' table. Implements parent and child recursive relationships."""
+
+    __tablename__ = 'przebiegi'
+
+    id = Column(Integer, primary_key=True)
+    start_semaphore = Column('semafor_poczatkowy', String)
+    end_semaphore = Column('semafor_koncowy', String, ForeignKey('przebiegi.semafor_poczatkowy'))
+
+    child = relationship('Passage', remote_side=[start_semaphore], uselist=False)
+    parent = relationship('Passage',
+                          backref=backref('child_back_rel', remote_side=[start_semaphore]),
+                          uselist=False)
+
+    def __init__(self, _id: int, start_semaphore: str, end_semaphore: str):
+        self.id = _id
+        self.start_semaphore = start_semaphore
+        self.end_semaphore = end_semaphore
---
+++ tests/models/test_passage.py
@@ -0,0 +1,91 @@
+from typing import TYPE_CHECKING
+from unittest import TestCase
+
+from sqlalchemy import create_engine, Table, MetaData, Integer, Column, String
+from sqlalchemy.orm import sessionmaker
+
+from models.passage import Passage
+
+if TYPE_CHECKING:
+    from sqlalchemy.engine import Engine
+    from sqlalchemy.orm import Session
+
+
+class TestPassage(TestCase):
+    engine: 'Engine'
+    session: 'Session'
+    table: Table
+    passage_1: Passage
+    passage_2: Passage
+
+    def _create_table(self):
+        metadata = MetaData(self.engine)
+        self.table = Table('przebiegi', metadata,
+                           Column('id', Integer, primary_key=True),
+                           Column('semafor_poczatkowy', String),
+                           Column('semafor_koncowy', String))
+        metadata.create_all()
+
+    def _drop_table(self):
+        self.table.drop()
+
+    def _add_passages(self):
+        self.passage_1 = Passage(1, 'semafor_1', 'semafor_2')
+        self.passage_2 = Passage(2, 'semafor_2', 'semafor_3')
+
+        self.session.add_all([self.passage_1, self.passage_2])
+        self.session.commit()
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.engine = create_engine('sqlite:///:memory:')
+        session = sessionmaker(cls.engine)
+        cls.session = session()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.session.close()
+        cls.engine.dispose()
+
+    def setUp(self) -> None:
+        self._create_table()
+        self._add_passages()
+
+    def tearDown(self) -> None:
+        self._drop_table()
+
+    def test_parent_relationship_none_for_non_parent_passage(self):
+        passage_id = 1
+        passage: Passage = self.session.query(Passage).filter_by(id=passage_id).first()
+
+        parent = passage.parent
+
+        self.assertIsNone(parent, 'Does not return None for non-parent passage')
+
+    def test_parent_relationship(self):
+        passage_id = 2
+        passage: Passage = self.session.query(Passage).filter_by(id=passage_id).first()
+        expected_passage = self.passage_1
+
+        parent = passage.parent
+
+        self.assertIsNotNone(parent, 'Does not return parent for passage with parent')
+        self.assertIs(expected_passage, parent, 'Parent does not equal the expected passage')
+
+    def test_child_relationship(self):
+        passage_id = 1
+        passage: Passage = self.session.query(Passage).filter_by(id=passage_id).first()
+        expected_passage = self.passage_2
+
+        child = passage.child
+
+        self.assertIsNotNone(child, 'Does not return child for passage with child')
+        self.assertIs(expected_passage, child, 'Child does not equal the expected passage')
+
+    def test_child_relationship_none_for_non_child_passage(self):
+        passage_id = 2
+        passage: Passage = self.session.query(Passage).filter_by(id=passage_id).first()
+
+        child = passage.child
+
+        self.assertIsNone(child, 'Does not return None for non-child passage')
