32bb86339dd9abbb25ce629f1a540306c60ccbdc
PWiener <patrykwiener@gmail.com>
2020-08-30 16:18:33 +0200

    added passage_service class

---
+++ services/passage_service.py
@@ -0,0 +1,40 @@
+"""This module contains PassageService responsible for Passage model class logic."""
+
+from typing import List
+
+from sqlalchemy.orm import Session
+
+from models.passage import Passage
+
+
+class PassageService:
+    """Represents service for Passage model class."""
+
+    def __init__(self, session: Session):
+        self._session = session
+
+    def get_all_root_passages(self) -> List[Passage]:
+        """Returns all root passages."""
+        return self._session.query(Passage) \
+            .filter_by(parent=None) \
+            .all()
+
+    @staticmethod
+    def get_linked_passages(root_passage: Passage, max_depth: int) -> List[Passage]:
+        """
+        Creates list of linked passages. Traverse recursively down the route hierarchy using
+        Passage recursive relationship.
+
+        :param root_passage: starting node
+        :param max_depth: traversing max depth through the hierarchy
+        :return: list of linked passages where the first element is the starting node
+        """
+
+        def get_linked_passages_internal(passage: Passage, array: List[Passage], curr_depth: int):
+            array.append(passage)
+            passage = passage.child
+            if not passage or curr_depth == max_depth:
+                return array
+            return get_linked_passages_internal(passage, array, curr_depth + 1)
+
+        return get_linked_passages_internal(root_passage, [], 1)
---
+++ tests/services/test_passage_service.py
@@ -0,0 +1,127 @@
+from typing import TYPE_CHECKING
+from unittest import TestCase
+
+from sqlalchemy import MetaData, Table, Column, Integer, String
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models.passage import Passage
+from services.passage_service import PassageService
+
+if TYPE_CHECKING:
+    from sqlalchemy.engine import Engine
+    from sqlalchemy.orm import Session
+
+
+class TestPassageService(TestCase):
+    engine: 'Engine'
+    session: 'Session'
+    passage_1: Passage
+    passage_2: Passage
+    passage_3: Passage
+
+    def _create_table(self):
+        metadata = MetaData(self.engine)
+        self.table = Table('przebiegi', metadata,
+                           Column('id', Integer, primary_key=True),
+                           Column('semafor_poczatkowy', String),
+                           Column('semafor_koncowy', String))
+        metadata.create_all()
+
+    def _drop_table(self):
+        self.table.drop()
+
+    def _add_passages(self):
+        self.passage_1 = Passage(1, 'semafor_1', 'semafor_2')
+        self.passage_2 = Passage(2, 'semafor_2', 'semafor_3')
+        self.passage_3 = Passage(3, 'semafor_6', 'semafor_7')
+
+        self.session.add_all([self.passage_1, self.passage_2, self.passage_3])
+        self.session.commit()
+
+    def _add_circular_passages(self):
+        self.passage_1 = Passage(1, 'semafor_1', 'semafor_2')
+        self.passage_2 = Passage(2, 'semafor_2', 'semafor_3')
+        self.passage_3 = Passage(3, 'semafor_3', 'semafor_1')
+
+        self.session.add_all([self.passage_1, self.passage_2, self.passage_3])
+        self.session.commit()
+
+    def _add_forked_passages(self):
+        self.passage_1 = Passage(1, 'semafor_1', 'semafor_2')
+        self.passage_2 = Passage(2, 'semafor_2', 'semafor_3')
+        self.passage_3 = Passage(3, 'semafor_2', 'semafor_4')
+
+        self.session.add_all([self.passage_1, self.passage_2, self.passage_3])
+        self.session.commit()
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.engine = create_engine('sqlite:///:memory:')
+        session = sessionmaker(cls.engine)
+        cls.session = session()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.session.close()
+        cls.engine.dispose()
+
+    def setUp(self) -> None:
+        self._create_table()
+        self._passage_service = PassageService(self.session)
+
+    def tearDown(self) -> None:
+        self._drop_table()
+
+    def test_get_all_root_passages(self):
+        self._add_passages()
+        expected = [self.passage_1, self.passage_3]
+
+        passages = self._passage_service.get_all_root_passages()
+
+        self.assertEqual(len(expected), len(passages), 'Returns to many passages')
+        self.assertIs(expected[0], passages[0], 'First root passage does not equal the expected one')
+        self.assertIs(expected[1], passages[1], 'Second root passage does not equal the expected one')
+
+    def test_get_all_root_passages_with_circular_route(self):
+        self._add_circular_passages()
+
+        passages = self._passage_service.get_all_root_passages()
+
+        self.assertFalse(passages, 'Root passage is not empty on circular route')
+
+    def test_get_linked_passages(self):
+        self._add_passages()
+        root = self.passage_1
+        max_depth = 4
+        expected = [self.passage_1, self.passage_2]
+
+        linked_passages = self._passage_service.get_linked_passages(root, max_depth)
+
+        self.assertEqual(len(expected), len(linked_passages), 'Returns to many passages')
+        self.assertEqual(expected[0], linked_passages[0], 'First passage does not equal the expected one')
+        self.assertEqual(expected[1], linked_passages[1], 'Second passage does not equal the expected one')
+
+    def test_get_linked_passages_with_exceeded_max_depth(self):
+        self._add_passages()
+        root = self.passage_1
+        max_depth = 1
+        expected = [self.passage_1]
+
+        linked_passages = self._passage_service.get_linked_passages(root, max_depth)
+
+        self.assertEqual(len(expected), len(linked_passages), 'Returns to many passages')
+        self.assertEqual(expected[0], linked_passages[0], 'First passage does not equal the expected one')
+
+    def test_get_linked_passages_with_fork_in_road(self):
+        self._add_forked_passages()
+        root = self.passage_1
+        max_depth = 4
+        expected = [self.passage_1, self.passage_2]
+        expected_length = len(expected)
+
+        passages = self._passage_service.get_linked_passages(root, max_depth)
+
+        self.assertEqual(len(passages), expected_length, 'Does not ignore forked passages')
+        self.assertIs(expected[0], passages[0], 'First root passage does not equal the expected one')
+        self.assertIs(expected[1], passages[1], 'Second root passage does not equal the expected one')
